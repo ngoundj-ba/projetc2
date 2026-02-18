a26a905ca27519a06fb661f131b0b71d15960aa0
ngoundj-ba <ngoundjb@gmail.com>
2026-02-18 14:11:53 +0000

    n entier negatif

---
+++ exoversionning.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+    int n,x,somme=0,cpt=0,i;
+    do{
+    printf("veuillez saisir n entier");
+    scanf("%d",&n);
+    }while(n<0);
+    for(i=0;i<n;i++){
+        do{
+            printf("saisir une valeur negatif");
+            scanf("%d",&x);
+        }while(n>0);
+        printf("le nombre que vous avez saisie est :%d",x);
+
+    }
+
+
+
+
+
+
+
+
+
+}
+
